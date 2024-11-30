b5f70ca184f8ae999e355a71d7aef6bc754a1a4f
phunghoangvnuit <phunghoangvnuit@gmail.com>
2024-11-30 11:31:13 +0700

    fe to be 005 - fix bug and finish create food form

--- DbQuery.sql
+++ DbQuery.sql
@@ -1,3 +1,13 @@
 CREATE DATABASE IF NOT EXISTS `topfood`;
 USE `topfood`;
 -- DROP DATABASE IF EXISTS `topfood`;
+
+-- test
+SELECT * FROM topfood.restaurant_images;
+
+DELETE FROM food_images
+WHERE food_id = 352;
+SELECT * FROM topfood.food_images;
+
+UPDATE your_table_name
+SET is_vegetarian = 0;
