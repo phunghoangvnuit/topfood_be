a3c80a3fd3108d9ad382fcb537bf42326d60ffc1
phunghoangvnuit <phunghoangvnuit@gmail.com>
2024-11-30 16:27:17 +0700

    finish base

--- src/main/java/com/fpt/topfood_be/TopfoodBeApplication.java
+++ src/main/java/com/fpt/topfood_be/TopfoodBeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TopfoodBeApplication {
-
+	// FPT APTECH - SEM4 - Group1 - T2301E
 	public static void main(String[] args) {
 		SpringApplication.run(TopfoodBeApplication.class, args);
 	}
