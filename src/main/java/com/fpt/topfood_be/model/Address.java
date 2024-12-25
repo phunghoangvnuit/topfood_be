ff112740e760fe2073e5fac62714d82c0581baae
phunghoangvnuit <phunghoangvnuit@gmail.com>
2024-12-25 22:56:28 +0700

    update address entity

--- src/main/java/com/fpt/topfood_be/model/Address.java
+++ src/main/java/com/fpt/topfood_be/model/Address.java
@@ -4,12 +4,26 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private String detailsAddress;
+
+    private String street;
+
+    private String ward;
+
+    private String district;
+
+    private String city;
+
+    private String country;
 
 }
