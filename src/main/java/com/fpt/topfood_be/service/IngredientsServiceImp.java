7e46df1eb9fc7c1a59a66265655934138c1334d8
phunghoangvnuit <phunghoangvnuit@gmail.com>
2025-01-03 16:33:19 +0700

    be & fe - mail ui update

--- src/main/java/com/fpt/topfood_be/service/IngredientsServiceImp.java
+++ src/main/java/com/fpt/topfood_be/service/IngredientsServiceImp.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class IngredientServiceImp implements IngredientsService{
+public class IngredientsServiceImp implements IngredientsService{
 
     @Autowired
     private IngredientItemRepository ingredientItemRepository;
